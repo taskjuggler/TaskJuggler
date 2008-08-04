88668b7980ed126c65919ac4a3d7c89ed7419be0
Chris Schlaeger <cs@kde.org>
2008-08-04 08:30:16 +0200

    More UTF-8 releated cleanups.

--- lib/UTF8String.rb
+++ lib/UTF8String.rb
@@ -8,17 +8,20 @@
 # published by the Free Software Foundation.
 #
 
-KCODE='UTF8'
+KCODE='u'
 require 'jcode'
 
 # This is an extension and modification of the standard String class. We do a
 # lot of UTF-8 character processing in the parser. Ruby 1.8 does not have good
 # enough UTF-8 support and Ruby 1.9 only handles UTF-8 characters as Strings.
 # This is very inefficient compared to representing them as Fixnum objects.
+# Some of these hacks can be removed once we have switched to 1.9 support
+# only.
 class String
 
   # Iteratate over the String calling the block for each UTF-8 character in
-  # the String.
+  # the String. This implementation looks more awkward but is noticeably
+  # faster than the often propagated regexp based implementations.
   def each_utf8_char
     c = 0
     length = 0
@@ -66,7 +69,9 @@ class String
     end
   end
 
-  # Return the number of UTF8 characters in the String
+  # Return the number of UTF8 characters in the String. We don't override the
+  # built-in length() function here as we don't know who else uses it for what
+  # purpose.
   def length_utf8
     len = 0
     each_utf8_char { |c| len += 1 }
