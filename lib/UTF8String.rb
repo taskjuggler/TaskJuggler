e56df891ef803bf6b0cd5348d1ef6c5d0b622ca0
Chris Schlaeger <cs@kde.org>
2009-02-17 08:16:52 +0100

    Ruby 1.9 has good UTF8 String support. Use the built-in functions.

--- lib/UTF8String.rb
+++ lib/UTF8String.rb
@@ -19,75 +19,82 @@ KCODE='u'
 # only.
 class String
 
-  # Iterate over the String calling the block for each UTF-8 character in
-  # the String. This implementation looks more awkward but is noticeably
-  # faster than the often propagated regexp based implementations.
-  def each_utf8_char
-    c = ''
-    length = 0
-    each_byte do |b|
-      c << b
-      if length > 0
-        # subsequent unicode byte
-        if (length -= 1) == 0
-          # end of unicode character reached
+  if RUBY_VERSION < '1.9.0'
+
+    # Iterate over the String calling the block for each UTF-8 character in
+    # the String. This implementation looks more awkward but is noticeably
+    # faster than the often propagated regexp based implementations.
+    def each_utf8_char
+      c = ''
+      length = 0
+      each_byte do |b|
+        c << b
+        if length > 0
+          # subsequent unicode byte
+          if (length -= 1) == 0
+            # end of unicode character reached
+            yield c
+            c = ''
+          end
+        elsif (b & 0xC0) == 0xC0
+          # first unicode byte
+          length = -1
+          while (b & 0x80) != 0
+            length += 1
+            b = b << 1
+          end
+        else
+          # ASCII character
           yield c
           c = ''
         end
-      elsif (b & 0xC0) == 0xC0
-        # first unicode byte
-        length = -1
-        while (b & 0x80) != 0
-          length += 1
-          b = b << 1
-        end
+      end
+    end
+
+    alias old_double_left_angle <<
+
+    # Replacement for the existing << operator that also works for characters
+    # above Fixnum 255 (UTF-8 characters).
+    def << (obj)
+      if obj.is_a?(String) || (obj < 256)
+        # In this case we can use the built-in concat.
+        concat(obj)
       else
-        # ASCII character
-        yield c
-        c = ''
+        # UTF-8 characters have a maximum length of 4 byte and no byte is 0.
+        mask = 0xFF000000
+        pos = 3
+        while pos >= 0
+          # Use the built-in concat operator for each byte.
+          concat((obj & mask) >> (8 * pos)) if (obj & mask) != 0
+          # Move mask and position to the next byte.
+          mask = mask >> 8
+          pos -= 1
+        end
       end
     end
-  end
 
-  alias old_double_left_angle <<
-
-  # Replacement for the existing << operator that also works for characters
-  # above Fixnum 255 (UTF-8 characters).
-  def << (obj)
-    if obj.is_a?(String) || (obj < 256)
-      # In this case we can use the built-in concat.
-      concat(obj)
-    else
-      # UTF-8 characters have a maximum length of 4 byte and no byte is 0.
-      mask = 0xFF000000
-      pos = 3
-      while pos >= 0
-        # Use the built-in concat operator for each byte.
-        concat((obj & mask) >> (8 * pos)) if (obj & mask) != 0
-        # Move mask and position to the next byte.
-        mask = mask >> 8
-        pos -= 1
-      end
+    # Return the number of UTF8 characters in the String. We don't override
+    # the built-in length() function here as we don't know who else uses it
+    # for what purpose.
+    def length_utf8
+      len = 0
+      each_utf8_char { |c| len += 1 }
+      len
     end
-  end
 
-  # Return the number of UTF8 characters in the String. We don't override the
-  # built-in length() function here as we don't know who else uses it for what
-  # purpose.
-  def length_utf8
-    len = 0
-    each_utf8_char { |c| len += 1 }
-    len
-  end
+    alias old_reverse reverse
 
-  alias old_reverse reverse
+    # UTF-8 aware version of reverse that replaces the built-in one.
+    def reverse
+      a = []
+      each_utf8_char { |c| a << c }
+      s = ''
+      a.reverse.join
+    end
 
-  # UTF-8 aware version of reverse that replaces the built-in one.
-  def reverse
-    a = []
-    each_utf8_char { |c| a << c }
-    s = ''
-    a.reverse.join
+  else
+    alias each_utf8_char each_char
+    alias length_utf8 length
   end
 
 end
