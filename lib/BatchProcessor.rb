f99d574f6b16b7a64e189e61e5af129580dd1722
Chris Schlaeger <cs@kde.org>
2009-02-19 22:05:09 +0100

    Documented the BatchProcess class.

--- lib/BatchProcessor.rb
+++ lib/BatchProcessor.rb
@@ -15,6 +15,9 @@ require 'monitor'
 
 class TaskJuggler
 
+  # The JobInfo class is just a storage container for some batch job realted
+  # pieces of information. It contains things like a job id, the process id,
+  # the stdout data and the like.
   class JobInfo
 
     attr_reader :jobId, :block, :tag
@@ -22,45 +25,89 @@ class TaskJuggler
                   :stderrP, :stderrC, :stderr
 
     def initialize(jobId, block, tag)
+      # The job id. A unique number that is used by the BatchProcessor objects
+      # to indentify jobs.
       @jobId = jobId
+      # This the the block of code to be run as external process.
       @block = block
+      # The tag can really be anything that the user of BatchProcessor needs
+      # to uniquely identify the job.
       @tag = tag
+      # The pipe to transfer stdout data from the child to the parent.
       @stdoutP, @stdoutC = IO.pipe
+      # The stdout output of the child
       @stdout = ''
+      # The pipe to transfer stderr data from the child to the parent.
       @stderrP, @stderrC = IO.pipe
+      # The stderr output of the child
       @stderr = ''
     end
 
   end
 
+  # The BatchProcessor class can be used to run code blocks of the program as
+  # a separate process. Mulitple pieces of code can be submitted to be
+  # executed in parallel. The number of CPU cores to use is limited at object
+  # creation time. The submitted jobs will be queued and scheduled to the
+  # given number of CPUs. The usage model is simple. Create an BatchProcessor
+  # object. use BatchProcessor#queue to submit all the jobs and then use
+  # BatchProcessor#wait to wait for completion and to process the results.
   class BatchProcessor
 
+    # Create a BatchProcessor object. +maxCpuCores+ limits the number of
+    # simultaneously spawned processes.
     def initialize(maxCpuCores)
       @maxCpuCores = maxCpuCores
+      # The job queue.
       @queue = Queue.new
-      @jobLock = Monitor.new
-      @pipes = []
-      @pipeToJob = {}
-      @pendingJobs = 0
-      @terminate = false
+      # A semaphore to guard accesses to shared data structures.`
+      @lock = Monitor.new
+      # A hash that maps process ids to JobInfo objects.
       @jobs = { }
+      # Counts the number of pending processes.
+      @pendingJobs = 0
+      # An Array that holds all the IO objects to receive data from.
+      @pipes = []
+      # A hash that maps IO objects to JobInfo objects
+      @pipeToJob = {}
+      # This global flag is set to true to signal the threads to terminate.
+      @terminate = false
+      # Sleep time of the threads when no data is pending.
       @timeout = 0.05
 
       Thread.abort_on_exception = true
+      # The JobInfo objects in the @queue are processed by the pusher thread.
+      # It forkes off processes to execute the code block associated with the
+      # JobInfo.
       @pusher = Thread.new { pusher }
+      # The popper thread waits for terminated childs and picks up the
+      # results.
       @popper = Thread.new { popper }
+      # The grabber thread collects $stdout and $stderr data from each child
+      # process and stores them in the corresponding JobInfo.
       @grabber = Thread.new { grabber }
     end
 
+    # Add a new job the job queue. +tag+ is some data that the caller can use
+    # to identify the job upon completion. +block+ is a Ruby code block to be
+    # executed in a separate process.
     def queue(tag = nil, &block)
+      # Create a new JobInfo object for the job and push it to the @queue.
       jobInfo = JobInfo.new(@queue.length, block, tag)
       @queue.push(jobInfo)
-      @pipes << jobInfo.stdoutP
-      @pipeToJob[jobInfo.stdoutP] = jobInfo
-      @pipes << jobInfo.stderrP
-      @pipeToJob[jobInfo.stderrP] = jobInfo
+      @lock.synchronize do
+        # Add the receiver end of the pipe to the @pipes Array.
+        @pipes << jobInfo.stdoutP
+        # Map the pipe end to this JobInfo object.
+        @pipeToJob[jobInfo.stdoutP] = jobInfo
+        # Same for $stderr.
+        @pipes << jobInfo.stderrP
+        @pipeToJob[jobInfo.stderrP] = jobInfo
+      end
     end
 
+    # Wait for all jobs to complete. The code block will get the JobInfo
+    # objects for each job to pick up the results.
     def wait
       while !@queue.empty? || @pendingJobs > 0 do
         sleep(@timeout)
@@ -73,53 +120,80 @@ class TaskJuggler
       @popper.join
       @grabber.join
 
+      # Pass the results of the jobs to the caller.
       @jobs.each_value { |job| yield(job) }
     end
 
     private
 
+    # This function runs in a separate thread to pop JobInfo items from the
+    # @queue and create child processes for them.
     def pusher
+      # Run until the terminate flag is set.
       until @terminate
         if @queue.empty? || @pendingJobs >= @maxCpuCores
+          # We have no jobs in the @queue or all CPU cores in use already.
           sleep(@timeout)
         else
+          # Get a new job from the @queue
           job = @queue.pop
           job.pid = fork do
+            # This is the child process now. Connect $stdout and $stderr to
+            # the pipes.
             $stdout.reopen(job.stdoutC)
             job.stdoutC.close
             $stderr.reopen(job.stderrC)
             job.stderrC.close
+            # Call the Ruby code block
             job.block.call
           end
-          @jobLock.synchronize do
+          @lock.synchronize do
+            # Save the process ID in the PID to JobInfo hash.
             @jobs[job.pid] = job
+            # Increase the jobs-in-flight counter.
             @pendingJobs += 1
           end
         end
       end
     end
 
+    # This function runs in a separate thread to wait for completed jobs. It
+    # waits for the process completion and stores the result in the
+    # corresponding JobInfo object.
     def popper
       until @terminate
         if @pendingJobs == 0
+          # No pending jobs, wait a bit.
           sleep(@timeout)
         else
+          # Wait for the next job to complete.
           pid, retVal = Process.wait2
-          @jobLock.synchronize do
+          @lock.synchronize do
+            # Get the JobInfo object that corresponds to the process ID.
             job = @jobs[pid]
             raise "Unknown job" if pid.nil?
+            # Save the return value.
             job.retVal = retVal
+            # We have one less job to worry about.
             @pendingJobs -= 1
           end
         end
       end
     end
 
+    # This function runs in a separate thread to pick up the $stdout and
+    # $stderr outputs of the child processes. It stores them in the JobInfo
+    # object that corresponds to each child process.
     def grabber
       until @terminate
+        # Wait for output in any of the pipes or a timeout.
         if (res = select(@pipes, nil, nil, @timeout))
+          # We have output data from at least one child. Check which pipe
+          # actually triggered the select.
           res[0].each do |pipe|
+            # Find the corresponding JobInfo object.
             job = @pipeToJob[pipe]
+            # Store the output.
             if pipe == job.stdoutP
               job.stdout << pipe.getc
             else
