d615e43e4fb84ccc1f06c0642b794dc5985179fc
Chris Schlaeger <cs@kde.org>
2009-03-01 18:20:00 +0100

    Fix tutorial project.
    Generate example/tutorial from the test case that is used for the
    manual.

---
+++ test/TestSuite/Syntax/Correct/manual2example.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby -w
+# encoding: UTF-8
+#
+# = manual2example.rb -- The TaskJuggler III Project Management Software
+#
+# Copyright (c) 2006, 2007, 2008, 2009 by Chris Schlaeger <cs@kde.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+
+def removeTags(fileName)
+  oFile = File.open('../../../../examples/' + fileName, 'w')
+  File.open(fileName, 'r') do |iFile|
+    while line = iFile.gets
+      oFile.puts line unless line =~ /^# \*\*\* EXAMPLE:/
+    end
+  end
+  oFile.close
+end
+
+removeTags('tutorial.tjp')
+
