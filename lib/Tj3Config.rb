073a75f0c3ceac3d2317ab5657cf3b17040097aa
Chris Schlaeger <cs@kde.org>
2009-10-20 07:34:58 +0200

    It's time for a new release.

--- lib/Tj3Config.rb
+++ lib/Tj3Config.rb
@@ -13,7 +13,7 @@
 require 'UTF8String'
 require 'AppConfig'
 
-AppConfig.version = '0.0.2'
+AppConfig.version = '0.0.3'
 AppConfig.packageName = 'TaskJuggler III'
 AppConfig.packageInfo = 'A Project Mangement Software'
 AppConfig.copyright = [ (2006..2009).to_a ]
