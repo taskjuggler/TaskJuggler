4b8cb6d2cdd6fc79b64cb52a3cace5ac6049372f
Chris Schlaeger <cs@kde.org>
2010-01-27 21:14:54 +0100

    It's 2010 now.

--- lib/Task.rb
+++ lib/Task.rb
@@ -3,7 +3,7 @@
 #
 # = Task.rb -- The TaskJuggler III Project Management Software
 #
-# Copyright (c) 2006, 2007, 2008, 2009 by Chris Schlaeger <cs@kde.org>
+# Copyright (c) 2006, 2007, 2008, 2009, 2010 by Chris Schlaeger <cs@kde.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 2 of the GNU General Public License as
