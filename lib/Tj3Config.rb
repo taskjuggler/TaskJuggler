1faf6afd62d970264938a9b3d01400ab73b40844
Chris Schlaeger <cs@kde.org>
2010-04-14 16:48:22 +0200

    Bumping version to 0.0.5.

--- lib/Tj3Config.rb
+++ lib/Tj3Config.rb
@@ -13,7 +13,7 @@
 require 'UTF8String'
 require 'AppConfig'
 
-AppConfig.version = '0.0.4'
+AppConfig.version = '0.0.5'
 AppConfig.packageName = 'taskjuggler'
 AppConfig.softwareName = 'TaskJuggler III'
 AppConfig.packageInfo = 'A Project Management Software'
