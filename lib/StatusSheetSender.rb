fb62d6e36f34167e479afd78abb598de0571e55b
Chris Schlaeger <cs@kde.org>
2010-09-22 13:08:39 +0200

    Include list of defaulters in the status report templates.
    If tj3ts_summary has been used to generate the 'missing-reports' file,
    this will now be included in the status sheet templates.

--- lib/StatusSheetSender.rb
+++ lib/StatusSheetSender.rb
@@ -30,6 +30,8 @@ class TaskJuggler
       @hideResource = '0'
       # The base directory of the status sheet templates.
       @templateDir = 'StatusSheetTemplates'
+      # The base directory of the received time sheets.
+      @timeSheetDir = 'TimeSheets'
       # This file contains the time intervals that the StatusSheetReceiver will
       # accept as a valid interval.
       @signatureFile = "#{@templateDir}/acceptable_intervals"
@@ -46,7 +48,55 @@ text, UTF-8 encoded and the status sheet header from 'statussheet' to the period
 end date must be in a single line that starts at the beginning of the line.
 
 EOT
+      # tj3ts_summary generates a list of resources that have not submitted
+      # their reports yet. If you want to generate the warning below, make
+      # sure you run tj3ts_summary immediately before you sent the status sheet
+      # templates.
+      defaulters = defaulterList
+      unless defaulters.empty?
+        @introText += <<"EOT"
+=============================== W A R N I N G ==============================
+The following people have not submitted their report yet. The status reports
+for the work they have done is not included in this template! You can either
+manually add their status to the tasks or asked them to send their time sheet
+immediately and re-request this template.
+
+#{defaulters.join}
+=============================== W A R N I N G ==============================
+
+EOT
+      end
+
       @mailSubject = "Your weekly status report template for %s"
+    end
+
+    def defaulterList
+      dirs = Dir.glob("#{@timeSheetDir}/????-??-??").sort
+      tsDir = nil
+      # The status sheet intervals and the time sheet intervals are not
+      # identical. The status sheet interval can be smaller and is somewhat
+      # later. But it always includes the end date of the corresponding time
+      # sheet period. To get the file with the IDs of the resources that have
+      # not submitted their report, we need to find the time sheet directory
+      # that is within the status sheet period.
+      repDate = Time.local(*@date.split('-'))
+      dirs.each do |dir|
+        dirDate = Time.local(*dir[-10..-1].split('-'))
+        if dirDate < repDate
+          tsDir = dir
+        else
+          break
+        end
+      end
+      # Check if there is a time sheet directory.
+      return [] unless tsDir
+
+      missingFile = "#{tsDir}/missing-reports"
+      # Check if it's got a missing-reports file.
+      return [] if !File.exists?(missingFile)
+
+      # Return the content of the file.
+      File.readlines(missingFile)
     end
 
   end
