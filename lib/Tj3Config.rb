544ac3437fca2af237782c9cf5bcbb6200bd54a9
Chris Schlaeger <cs@kde.org>
2011-01-31 10:57:02 +0100

    Bumping version to 0.0.10
    0.0.9 was a no-go version.

--- lib/Tj3Config.rb
+++ lib/Tj3Config.rb
@@ -14,7 +14,7 @@
 require 'UTF8String'
 require 'AppConfig'
 
-AppConfig.version = '0.0.9'
+AppConfig.version = '0.0.10'
 AppConfig.packageName = 'taskjuggler'
 AppConfig.softwareName = 'TaskJuggler III'
 AppConfig.packageInfo = 'A Project Management Software'
