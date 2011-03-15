4d2c687c1b4bfab93ef70a55e7f17e3d5938f160
Chris Schlaeger <chris@linux.com>
2011-03-16 04:01:47 +0800

    All programs now have a proper entry class that is derived of off Tj3AppBase

--- lib/tj3ss_receiver.rb
+++ lib/tj3ss_receiver.rb
@@ -11,47 +11,7 @@
 # published by the Free Software Foundation.
 #
 
-require 'taskjuggler/Tj3SheetAppBase'
-require 'taskjuggler/StatusSheetReceiver'
-
-# Name of the application
-AppConfig.appName = 'tj3ss_receiver'
-
-class TaskJuggler
-
-  class Tj3SsReceiver < Tj3SheetAppBase
-
-    def initialize
-      super
-    end
-
-    def processArguments(argv)
-      super do
-        @opts.banner += <<'EOT'
-This program can be used to receive filled-out status sheets via email.
-It reads the emails from STDIN and extracts the status sheet from the
-attached files. The status sheet is checked for correctness. Good status
-sheets are filed away. The sender be informed by email that the status
-sheets was accepted or rejected.
-EOT
-      end
-    end
-
-    def main
-      super
-      ts = TaskJuggler::StatusSheetReceiver.new('tj3ss_receiver')
-      @rc.configure(ts, 'global')
-      @rc.configure(ts, 'statussheets')
-      @rc.configure(ts, 'statussheets.receiver')
-      ts.workingDir = @workingDir if @workingDir
-      ts.dryRun = @dryRun
-
-      ts.processEmail
-      0
-    end
-
-  end
-
-end
+require 'taskjuggler/apps/Tj3SsReceiver'
 
 exit TaskJuggler::Tj3SsReceiver.new.main()
+
--- lib/tj3ts_sender.rb
+++ lib/tj3ts_sender.rb
@@ -11,69 +11,7 @@
 # published by the Free Software Foundation.
 #
 
-# This script is used to send out the time sheet templates to the employees.
-# It should be run from a cron job once a week.
-
-require 'taskjuggler/Tj3SheetAppBase'
-require 'taskjuggler/TimeSheetSender'
-
-# Name of the application suite
-AppConfig.appName = 'tj3ts_sender'
-
-class TaskJuggler
-
-  class Tj3TsSender < Tj3SheetAppBase
-
-    def initialize
-      super
-      @optsSummaryWidth = 22
-      @force = false
-      @intervalDuration = nil
-
-      # The default report period end is next Monday 0:00.
-      @date = TjTime.new.nextDayOfWeek(1).to_s('%Y-%m-%d')
-      @resourceList = []
-    end
-
-    def processArguments(argv)
-      super do
-        @opts.banner += <<'EOT'
-This program can be used to send out time sheets templates via email. It will
-generate time sheet templates for all resources of the project. The project
-data will be accesses via tj3client from a running TaskJuggler server process.
-EOT
-        @opts.on('-r', '--resource <ID>', String,
-                format('Only generate template for given resource')) do |arg|
-          @resourceList << arg
-        end
-        @opts.on('-f', '--force',
-                format('Send out a new template even if one exists ' +
-                       'already')) do |arg|
-          @force = true
-        end
-        optsEndDate
-      end
-    end
-
-    def main
-      super
-      ts = TimeSheetSender.new('tj3ts_sender')
-      @rc.configure(ts, 'global')
-      @rc.configure(ts, 'timesheets')
-      @rc.configure(ts, 'timesheets.sender')
-      ts.workingDir = @workingDir if @workingDir
-      ts.dryRun = @dryRun
-      ts.force = @force
-      ts.intervalDuration = @intervalDuration if @intervalDuration
-      ts.date = @date if @date
-
-      ts.sendTemplates(@resourceList)
-      0
-    end
-
-  end
-
-end
+require 'taskjuggler/apps/Tj3TsSender'
 
 exit TaskJuggler::Tj3TsSender.new.main()
 
