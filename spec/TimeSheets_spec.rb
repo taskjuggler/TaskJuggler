19c300f1dc4df992f4a5f0b06f3349c31f477412
Chris Schlaeger <chris@linux.com>
2011-04-02 04:10:36 +0200

    Improving time sheet tests.

--- spec/TimeSheets_spec.rb
+++ spec/TimeSheets_spec.rb
@@ -15,10 +15,12 @@ require 'rubygems'
 require 'fileutils'
 require 'support/DaemonControl'
 require 'taskjuggler/apps/Tj3TsSender'
+require 'taskjuggler/apps/Tj3TsReceiver'
+require 'taskjuggler/apps/Tj3TsSummary'
 
 class TaskJuggler
 
-  describe TimeSheetSender do
+  describe TimeSheets do
 
     include DaemonControl
     include FileUtils
@@ -27,6 +29,7 @@ class TaskJuggler
       # Make sure we run in the same directory as the spec file.
       @pwd = pwd
       cd(File.dirname(__FILE__))
+      ENV['TASKJUGGLER_DATA_PATH'] = "../"
 
       cleanup
       startDaemon(<<'EOT'
@@ -76,11 +79,93 @@ EOT
       end
       raise "Can't load project" unless res.returnValue == 0
 
-      @res = stdIoWrapper do
+      res = stdIoWrapper do
         Tj3TsSender.new.main(%w( --dryrun --silent -e 2011-03-21 ))
       end
-      @mails = collectMails(@res.stdOut)
-      raise "Timesheet generation failed" unless @res.returnValue == 0
+      @tss_mails = collectMails(res.stdOut)
+      raise "Timesheet generation failed" unless res.returnValue == 0
+
+      @sheet1 = <<'EOT'
+# --------8<--------8<--------
+timesheet r1 2011-03-14-00:00-+0000 - 2011-03-21-00:00-+0000 {
+  task t1 {
+    work 30.0%
+    remaining 2.0d
+    status red "More work" {
+      details -8<-
+      This is more work than expected.
+      ->8-
+    }
+  }
+  task t2 {
+    work 50.0%
+    remaining 0.0d
+    status green "All work done!"
+  }
+
+  newtask t4 "A new job" {
+    work 20%
+    remaining 1.0d
+    status green "May be a good idea" {
+      summary -8<-
+      I thought this might be useful work.
+      ->8-
+    }
+  }
+}
+# -------->8-------->8--------
+EOT
+      @sheet2 = <<'EOT'
+# --------8<--------8<--------
+timesheet r2 2011-03-14-00:00-+0000 - 2011-03-21-00:00-+0000 {
+  # Task: T3
+  task t3 {
+    work 100.0%
+    remaining 5.0d
+    status green "What a job!"
+  }
+
+  status yellow "I'm not feeling good!" {
+    summary -8<-
+    We all live on a yellow submarine!
+    ->8-
+  }
+}
+# -------->8-------->8--------
+EOT
+      @tsr_mails = []
+      [ @sheet1, @sheet2 ].each do |sheet|
+        mail = Mail.new do
+          subject "Timesheet"
+          content_type [ 'text', 'plain', { 'charset' => 'UTF-8' } ]
+          content_transfer_encoding 'base64'
+          body sheet.to_base64
+        end
+        mail.to = 'taskjuggler@example.com'
+        mail.from 'r@example.com'
+        res = stdIoWrapper(mail.to_s) do
+          Tj3TsReceiver.new.main(%w( --dryrun --silent ))
+        end
+        @tsr_mails += collectMails(res.stdOut)
+        unless res.returnValue == 0
+          raise "Timesheet processing failed: #{res.stdErr}"
+        end
+      end
+
+      res = stdIoWrapper(prj) do
+        Tj3Client.new.main(%w( --unsafe --silent add . TimeSheets/2011-03-21/all.tji ))
+      end
+      unless res.returnValue == 0
+        raise "Project reloading failed: #{res.stdErr}"
+      end
+
+      res = stdIoWrapper do
+        Tj3TsSummary.new.main(%w( --dryrun --silent -e 2011-03-21 ))
+      end
+      @sum_mails = collectMails(res.stdOut)
+      unless res.returnValue == 0
+        raise "Summary generation failed: #{res.stdErr}"
+      end
     end
 
     after(:all) do
@@ -89,36 +174,86 @@ EOT
       cd(@pwd)
     end
 
-    it 'should have generated 2 mails' do
-      @mails.length.should == 2
-    end
+    describe TimeSheetSender do
 
-    it 'should have email sender foo@example.com' do
-      @mails.each do |mail|
-        mail.from[0].should == 'foo@example.com'
+      it 'should have generated 2 mails' do
+        @tss_mails.length.should == 2
       end
-    end
 
-    it 'should have proper email receivers' do
-      @mails[0].to[0].should == 'r1@example.com'
-      @mails[1].to[0].should == 'r2@example.com'
-    end
-
-    it 'should generate properly dated headers' do
-      countLines(@mails[0].parts[0].decoded,
-                 'timesheet r1 2011-03-14-00:00-+0000 - ' +
-                 '2011-03-21-00:00-+0000').should == 1
-      countLines(@mails[1].parts[0].decoded,
-                 'timesheet r2 2011-03-14-00:00-+0000 - ' +
-                 '2011-03-21-00:00-+0000').should == 1
-    end
-
-    it 'should have matching timesheets in body and attachment' do
-      @mails.each do |mail|
-        bodySheet = extractTimeSheet(mail.parts[0].decoded)
-        attachedSheet = extractTimeSheet(mail.part[1].decoded)
-        bodySheet.should == attachedSheet
+      it 'should have email sender foo@example.com' do
+        @tss_mails.each do |mail|
+          mail.from[0].should == 'foo@example.com'
+        end
       end
+
+      it 'should have proper email receivers' do
+        @tss_mails[0].to[0].should == 'r1@example.com'
+        @tss_mails[1].to[0].should == 'r2@example.com'
+      end
+
+      it 'should generate properly dated headers' do
+        countLines(@tss_mails[0].parts[0].decoded,
+                   'timesheet r1 2011-03-14-00:00-+0000 - ' +
+                   '2011-03-21-00:00-+0000').should == 1
+        countLines(@tss_mails[1].parts[0].decoded,
+                   'timesheet r2 2011-03-14-00:00-+0000 - ' +
+                   '2011-03-21-00:00-+0000').should == 1
+      end
+
+      it 'should have matching timesheets in body and attachment' do
+        @tss_mails.each do |mail|
+          bodySheet = extractTimeSheet(mail.parts[0].decoded)
+          attachedSheet = extractTimeSheet(mail.part[1].decoded)
+          bodySheet.should == attachedSheet
+        end
+      end
+
+    end
+
+    describe TimeSheetReceiver do
+
+      it 'should have generated 2 mails' do
+        @tsr_mails.length.should == 2
+      end
+
+      it 'should have email sender foo@example.com' do
+        @tsr_mails.each do |mail|
+          mail.from[0].should == 'foo@example.com'
+        end
+      end
+
+      it 'should have proper email receivers' do
+        @tsr_mails[0].to[0].should == 'r1@example.com'
+        @tsr_mails[1].to[0].should == 'r2@example.com'
+      end
+
+      it 'should have stored timesheets' do
+        @sheet1.should == File.read('TimeSheets/2011-03-21/r1_2011-03-21.tji')
+        @sheet2.should == File.read('TimeSheets/2011-03-21/r2_2011-03-21.tji')
+      end
+
+    end
+
+    describe TimeSheetSummary do
+
+      it 'should have generated 4 mails' do
+        @sum_mails.length.should == 4
+      end
+
+      it 'should have proper email receivers' do
+        @sum_mails[0].to[0].should == 'archive@example.com'
+        @sum_mails[1].to[0].should == 'archive@example.com'
+        @sum_mails[2].to[0].should == 'archive@example.com'
+        @sum_mails[3].to[0].should == 'crew@example.com'
+      end
+
+      it 'should have proper email senders' do
+        @sum_mails[0].from[0].should == 'r1@example.com'
+        @sum_mails[1].from[0].should == 'r2@example.com'
+        @sum_mails[2].from[0].should == 'foo@example.com'
+        @sum_mails[3].from[0].should == 'foo@example.com'
+      end
+
     end
 
     private
@@ -170,7 +305,7 @@ EOT
     end
 
     def cleanup
-      rm_rf %w( TimeSheetTemplates timesheets.log tj3.log tj3ts_sender.log )
+      rm_rf %w( TimeSheetTemplates TimeSheets timesheets.log tj3.log tj3ts_sender.log tj3ts_receiver.log )
     end
 
   end
