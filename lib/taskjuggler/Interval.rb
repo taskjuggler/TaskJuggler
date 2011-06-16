00052a8f54795dd114cb306ae4562ac9deea1e7a
Chris Schlaeger <chris@linux.com>
2011-06-16 08:17:05 +0200

    Factor our Interval base class.

--- lib/taskjuggler/Interval.rb
+++ lib/taskjuggler/Interval.rb
@@ -15,69 +15,35 @@ require 'taskjuggler/TjTime'
 
 class TaskJuggler
 
-  # The TimeInterval class provides objects that model a time interval. The
-  # start end end time are represented as seconds after Jan 1, 1970. The start
-  # is part of the interval, the end is not.
-  class TimeInterval
+  # This is the based class used to store several kinds of intervals in
+  # derived classes.
+  class Interval
 
-    attr_accessor :start, :end
-
-    # Create a new TimeInterval. _args_ can be three different kind of arguments.
-    #
-    # a and b should be TjTime objects.
-    #
-    # TimeInterval.new(a, b)  | -> Interval(a, b)
-    # TimeInterval.new(a)     | -> Interval(a, a)
-    # TimeInterval.new(iv)    | -> Interval(iv.start, iv.end)
-    #
-    def initialize(*args)
-      if args.length == 1
-        if args[0].is_a?(TjTime)
-          # Just one argument, a date
-          @start = @end = args[0]
-        elsif args[0].is_a?(TimeInterval)
-          # Just one argument, a TimeInterval
-          @start = args[0].start
-          @end = args[0].end
-        else
-          raise "Illegal argument"
-        end
-      elsif args.length == 2
-        # Two arguments, a start and end date
-        @start = args[0]
-        @end = args[1]
-        raise "Interval start must be a date" unless @start.is_a?(TjTime)
-        raise "Interval end must be a date" unless @end.is_a?(TjTime)
-        if @end < @start
-          raise "Invalid interval"
-        end
-      else
-        raise "Illegal arguments"
+    def initialize(s, e)
+      @start = s
+      @end = e
+      if @end < @start
+        raise "Invalid interval"
       end
-    end
-
-    # Return the duration of the TimeInterval.
-    def duration
-      @end - @start
     end
 
     # Return true if _arg_ is contained within the Interval. It can either
     # be a single TjTime or another Interval.
     def contains?(arg)
-      if arg.is_a?(TjTime)
-        return @start <= arg && arg < @end
-      else
+      if arg.is_a?(Interval)
         return @start <= arg.start && arg.end <= @end
+      else
+        return @start <= arg && arg < @end
       end
     end
 
     # Check whether the Interval _arg_ overlaps with this Interval.
     def overlaps?(arg)
-      if arg.is_a?(TjTime)
-        return @start <= arg && arg < @end
-      else
+      if arg.is_a?(Interval)
         return (@start <= arg.start && arg.start < @end) ||
                (arg.start <= @start && @start < arg.end)
+      else
+        return @start <= arg && arg < @end
       end
     end
 
@@ -86,7 +52,7 @@ class TaskJuggler
     def intersection(iv)
       newStart = @start > iv.start ? @start : iv.start
       newEnd = @end < iv.end ? @end : iv.end
-      newStart < newEnd ? TimeInterval.new(newStart, newEnd) : nil
+      newStart < newEnd ? self.class.new(newStart, newEnd) : nil
     end
 
     # Append or prepend the Interval _iv_ to self. If _iv_ does not directly
@@ -94,10 +60,10 @@ class TaskJuggler
     def combine(iv)
       if iv.end == @start
         # Prepend iv
-        Array.new TimeInterval(iv.start, @end)
+        Array.new self.class.new(iv.start, @end)
       elsif @end == iv.start
         # Append iv
-        Array.new TimeInterval(@start, iv.end)
+        Array.new self.class.new(@start, iv.end)
       else
         self
       end
@@ -117,6 +83,49 @@ class TaskJuggler
     # Return true if the Interval _iv_ describes an identical time period.
     def ==(iv)
       @start == iv.start && @end == iv.end
+    end
+
+  end
+
+  # The TimeInterval class provides objects that model a time interval. The
+  # start end end time are represented as seconds after Jan 1, 1970. The start
+  # is part of the interval, the end is not.
+  class TimeInterval < Interval
+
+    attr_accessor :start, :end
+
+    # Create a new TimeInterval. _args_ can be three different kind of arguments.
+    #
+    # a and b should be TjTime objects.
+    #
+    # TimeInterval.new(a, b)  | -> Interval(a, b)
+    # TimeInterval.new(a)     | -> Interval(a, a)
+    # TimeInterval.new(iv)    | -> Interval(iv.start, iv.end)
+    #
+    def initialize(*args)
+      if args.length == 1
+        if args[0].is_a?(TjTime)
+          # Just one argument, a date
+          super(args[0], args[0])
+        elsif args[0].is_a?(TimeInterval)
+          # Just one argument, a TimeInterval
+          super(args[0].start, args[0].end)
+        else
+          raise "Illegal argument"
+        end
+      elsif args.length == 2
+        # Two arguments, a start and end date
+        super(args[0], args[1])
+        raise "Interval start must be a date" unless @start.is_a?(TjTime)
+        raise "Interval end must be a date" unless @end.is_a?(TjTime)
+      else
+        raise "Illegal arguments"
+      end
+    end
+
+    # Return the duration of the TimeInterval.
+    def duration
+      @end - @start
     end
 
     # Turn the TimeInterval into a human readable form.
