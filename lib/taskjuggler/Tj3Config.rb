87bbd4134af06a6b1b0995c047665aaa821bbe56
Chris Schlaeger <chris@linux.com>
2011-07-10 12:18:11 +0200

    Bumping version to 0.2.0

--- lib/taskjuggler/Tj3Config.rb
+++ lib/taskjuggler/Tj3Config.rb
@@ -14,7 +14,7 @@
 require 'taskjuggler/UTF8String'
 require 'taskjuggler/AppConfig'
 
-AppConfig.version = '0.1.1'
+AppConfig.version = '0.2.0'
 AppConfig.packageName = 'taskjuggler'
 AppConfig.softwareName = 'TaskJuggler III'
 AppConfig.packageInfo = 'A Project Management Software'
