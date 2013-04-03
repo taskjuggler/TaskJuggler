716a41efa4c717b59f842528e7c709908efd46d3
Chris Schlaeger <chris@linux.com>
2013-04-03 20:53:39 +0200

    It's 2013 now.

--- lib/taskjuggler/TjpSyntaxRules.rb
+++ lib/taskjuggler/TjpSyntaxRules.rb
@@ -3,7 +3,7 @@
 #
 # = TjpSyntaxRules.rb -- The TaskJuggler III Project Management Software
 #
-# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012
+# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013
 #               by Chris Schlaeger <chris@linux.com>
 #
 # This program is free software; you can redistribute it and/or modify
