44b7f9bec322a1984cd9a111fae51e9c39d713ef
Chris Schlaeger <chris@linux.com>
2014-03-19 22:00:02 +0100

    Updating copyright to 2014

--- lib/taskjuggler/Tj3Config.rb
+++ lib/taskjuggler/Tj3Config.rb
@@ -3,8 +3,8 @@
 #
 # = Tj3Config.rb -- The TaskJuggler III Project Management Software
 #
-# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013
-#               by Chris Schlaeger <chris@linux.com>
+# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014
+#               by Chris Schlaeger <cs@taskjuggler.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 2 of the GNU General Public License as
@@ -18,8 +18,8 @@ AppConfig.version = '3.5.0'
 AppConfig.packageName = 'taskjuggler'
 AppConfig.softwareName = 'TaskJuggler'
 AppConfig.packageInfo = 'A Project Management Software'
-AppConfig.copyright = [ (2006..2013).to_a ]
-AppConfig.authors = [ 'Chris Schlaeger <chris@linux.com>' ]
+AppConfig.copyright = [ (2006..2014).to_a ]
+AppConfig.authors = [ 'Chris Schlaeger <cs@taskjuggler.org>' ]
 AppConfig.contact = 'http://www.taskjuggler.org'
 AppConfig.license = <<'EOT'
 This program is free software; you can redistribute it and/or modify it under
--- spec/IntervalList_spec.rb
+++ spec/IntervalList_spec.rb
@@ -3,8 +3,8 @@
 #
 # = IntervalList_spec.rb -- The TaskJuggler III Project Management Software
 #
-# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013
-#               by Chris Schlaeger <chris@linux.com>
+# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014
+#               by Chris Schlaeger <cs@taskjuggler.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 2 of the GNU General Public License as
