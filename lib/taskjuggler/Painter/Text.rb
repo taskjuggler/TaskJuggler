f77afd9007af26e725cd87a5960e64e3ee627844
jostber <jbernts@broadpark.no>
2015-03-02 23:01:54 +0100

    Update copyright dates and mail address

--- lib/taskjuggler/Painter/Text.rb
+++ lib/taskjuggler/Painter/Text.rb
@@ -4,7 +4,7 @@
 # = Text.rb -- The TaskJuggler III Project Management Software
 #
 # Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012
-#               by Chris Schlaeger <chris@linux.com>
+#               by Chris Schlaeger <cs@taskjuggler.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 2 of the GNU General Public License as
--- lib/taskjuggler/Query.rb
+++ lib/taskjuggler/Query.rb
@@ -3,7 +3,7 @@
 #
 # = Query.rb -- The TaskJuggler III Project Management Software
 #
-# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014
+# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015
 #               by Chris Schlaeger <cs@taskjuggler.org>
 #
 # This program is free software; you can redistribute it and/or modify
