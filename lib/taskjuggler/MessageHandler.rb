68a0dc7174678d63dc97949e02b15490647600af
Chris Schlaeger <chris@linux.com>
2022-10-03 20:55:27 +0200

    Fix: Eliminate use of useless variable

--- lib/taskjuggler/MessageHandler.rb
+++ lib/taskjuggler/MessageHandler.rb
@@ -248,7 +248,6 @@ class TaskJuggler
 
       timeStamp = Time.new.strftime("%Y-%m-%d %H:%M:%S")
       begin
-        @logFile
         File.open(@logFile, 'a') do |f|
           f.write("#{timeStamp} #{type} #{@appName}[#{Process.pid}]: " +
                   "#{message}\n")
